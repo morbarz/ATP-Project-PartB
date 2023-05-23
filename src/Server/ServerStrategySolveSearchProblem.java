1c3367448e82c7e147ef6bcb461231a58284b344
morbarz <morbarz@post.bgu.ac.il>
2023-05-23 17:23:41 +0300

    23/5

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -1,4 +1,22 @@
 package Server;
 
-public class ServerStrategySolveSearchProblem {
-}
+import algorithms.mazeGenerators.Maze;
+import algorithms.search.ASearchingAlgorithm;
+import algorithms.search.Solution;
+import test.RunSearchOnMaze;
+
+import java.io.*;
+
+public class ServerStrategySolveSearchProblem implements IServerStrategy {
+    public void applyStrategy(InputStream inFromClient, OutputStream outToClient) throws IOException, ClassNotFoundException {
+        try {
+            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
+            ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
+
+            Maze maze = (Maze) fromClient.readObject();
+            Solution news = ASearchingAlgorithm;
+            news.getSolutionPath();
+
+
+
+        }
