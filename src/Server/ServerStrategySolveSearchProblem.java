0069540adab20a80e11ca7475b2b6a32df134acb
morbarz <morbarz@post.bgu.ac.il>
2023-05-23 17:38:07 +0300

    23/5

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -2,6 +2,8 @@ package Server;
 
 import algorithms.mazeGenerators.Maze;
 import algorithms.search.ASearchingAlgorithm;
+import algorithms.search.BestFirstSearch;
+import algorithms.search.SearchableMaze;
 import algorithms.search.Solution;
 import test.RunSearchOnMaze;
 
@@ -14,8 +16,12 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy {
             ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
 
             Maze maze = (Maze) fromClient.readObject();
-            Solution news = ASearchingAlgorithm;
-            news.getSolutionPath();
+            SearchableMaze searchableMaze = new SearchableMaze(maze);
+            BestFirstSearch bfs = new BestFirstSearch();
+            Solution solution = bfs.solve(searchableMaze);
+            toClient.writeObject(solution);
+
+
 
 
 
