506972fe77a5fe39da09ae3880613e06e1f90014
NivSampson <129731047+NivSampson@users.noreply.github.com>
2023-05-23 17:24:55 +0300

    ז

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -1,22 +1,7 @@
 package Server;
 
-import algorithms.mazeGenerators.Maze;
-import algorithms.search.ASearchingAlgorithm;
-import algorithms.search.Solution;
-import test.RunSearchOnMaze;
-
-import java.io.*;
-
-public class ServerStrategySolveSearchProblem implements IServerStrategy {
-    public void applyStrategy(InputStream inFromClient, OutputStream outToClient) throws IOException, ClassNotFoundException {
-        try {
-            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
-            ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
-
-            Maze maze = (Maze) fromClient.readObject();
-            Solution news = ASearchingAlgorithm;
-            news.getSolutionPath();
+public class ServerStrategySolveSearchProblem
+{
 
 
-
-        }
+}
