51a317b25dd0edf8124ff72362d76ceab37e1318
NivSampson <129731047+NivSampson@users.noreply.github.com>
2023-05-26 14:18:58 +0300

    ז

--- src/Server/Configurations.java
+++ src/Server/Configurations.java
@@ -12,39 +12,5 @@ public class Configurations {
 
 
     private Configurations(){}
-
-    public static Configurations createInstance(){
-        if (properties_file != null){
-            System.out.println("Properties File already exist");
-        }
-        else {
-            properties_file = new Configurations();
-        }
-        return properties_file;
-    }
-
-
-    public void creatPropFile(){
-        try{
-            inStream = getClass().getClassLoader().getResourceAsStream("config.properties");
-            properties = new Properties();
-
-            outStream = new FileOutputStream("resources/config.properties");
-            properties.load(inStream);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    public synchronized Properties readConfig(){
-        try {
-            properties = new Properties();
-            inStream = new FileInputStream("resources\\config.properties");
-            properties.load(inStream);
-            return properties;
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
 }
+
--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -1,3 +1,4 @@
+
 package Server;
 
 import algorithms.mazeGenerators.Maze;
