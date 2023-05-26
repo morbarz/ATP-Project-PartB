dc46c984632ca6ea33e738209c9552f07d770911
NivSampson <129731047+NivSampson@users.noreply.github.com>
2023-05-26 14:09:30 +0300

    ז

--- src/Server/Configurations.java
+++ src/Server/Configurations.java
@@ -1,4 +1,50 @@
 package Server;
 
+import java.io.*;
+import java.util.Properties;
+
 public class Configurations {
+    private static Configurations properties_file = null;
+    private Properties properties;
+    private InputStream inStream;
+    private OutputStream outStream;
+
+
+
+    private Configurations(){}
+
+    public static Configurations createInstance(){
+        if (properties_file != null){
+            System.out.println("Properties File already exist");
+        }
+        else {
+            properties_file = new Configurations();
+        }
+        return properties_file;
+    }
+
+
+    public void creatPropFile(){
+        try{
+            inStream = getClass().getClassLoader().getResourceAsStream("config.properties");
+            properties = new Properties();
+
+            outStream = new FileOutputStream("resources/config.properties");
+            properties.load(inStream);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized Properties readConfig(){
+        try {
+            properties = new Properties();
+            inStream = new FileInputStream("resources\\config.properties");
+            properties.load(inStream);
+            return properties;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
